e9a6d42a24076b572acddbb31cac999aff158b3d
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-01-24 14:22:38 -0600

    question format defined

---
+++ script.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        question: "what is HTML?",
+        option_A: "food",
+        option_B: "shelter",
+        option_C: "right",
+        option_D: "42",
+        correct: "C"
+    },
+    {
+        question: "what is the answer to the life, universe and everything else?",
+        option_A: "food",
+        option_B: "shelter",
+        option_C: "water",
+        option_D: "42",
+        correct: "D"
+    }
+]
+
+
