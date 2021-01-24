226f6a3fe610908703d621e584c9b43c37fde680
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-01-24 15:00:27 -0600

    define function to render question, set paramaters for question object

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 var questions = [
     {
         question: "what is HTML?",
-        option_A: "food",
-        option_B: "shelter",
-        option_C: "right",
-        option_D: "42",
+        options: [
+            {label: "A", value: "house"},
+            {label: "B", value: "metal"},
+            {label: "C", value: "tool"},
+            {label: "D", value: "loop"},
+
+        ],
         correct: "C"
     },
     {
         question: "what is the answer to the life, universe and everything else?",
-        option_A: "food",
-        option_B: "shelter",
-        option_C: "water",
-        option_D: "42",
+        options: [
+            {label: "A", value: "food"},
+            {label: "B", value: "shelter"},
+            {label: "C", value: "water"},
+            {label: "D", value: "42"},
+        ],
         correct: "D"
     }
 ]
 
 function renderQuestion(question){
-    
+    //question wiil be text newline, ul where li is option, each li is clickable
+    var q = $("<p></p>").html(question.question);
+    var ul = $("<ul></ul>");
+    for (let index = 0; index < question.options.length; index++) {
+        const option = question.options[index];
+        var li = $("<li></li>").attr("label", option.label).attr("class", "option").html(option.value);
+        ul.append(li)
+    }
+    $("#question").html("").append(q).append(ul)
 };
+renderQuestion(questions[1])
